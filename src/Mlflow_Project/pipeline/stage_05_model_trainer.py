ca7611967e9210e82e0f1f76f4303bacb0148dcd
Sengarofficial <grv.m02@gmail.com>
2023-10-13 16:32:39 +0530

    Pipeline Model Trainer

---
+++ src/Mlflow_Project/pipeline/stage_05_model_trainer.py
@@ -0,0 +1,31 @@
+from src.Mlflow_Project.config.configuration import ConfigurationManager
+from src.Mlflow_Project.entity.config_entity import ModelTrainerConfig
+from src.Mlflow_Project.components.model_trainer import ModelTrainer
+from src.Mlflow_Project import logger 
+
+
+STAGE_NAME = "Model Trainer Stage"
+
+class ModelTrainerTrainingPipeline:
+    def __init__(self):
+        pass 
+
+
+
+    def main(self):
+        config = ConfigurationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer_config = ModelTrainer(config = model_trainer_config)
+        model_trainer_config.train()
+
+
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<")
+        obj = ModelTrainerTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<")
+    except Exception as e:
+        logger.exception(e)
