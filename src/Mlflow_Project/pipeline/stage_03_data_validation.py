d8c380521ddec7aef8f525268e573df6d27c2f21
Sengarofficial <grv.m02@gmail.com>
2023-10-13 16:29:25 +0530

    Pipeline Data Validation

---
+++ src/Mlflow_Project/pipeline/stage_03_data_validation.py
@@ -0,0 +1,28 @@
+# update the pipeline 
+
+from src.Mlflow_Project.config.configuration import ConfigurationManager
+from src.Mlflow_Project.components.data_validation import DataValidation
+from src.Mlflow_Project import logger 
+
+STAGE_NAME = "Data Validation Stage"
+
+class DataIngestionValidationPipeline:
+    def __init__(self):
+        pass 
+
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValidation(config = data_validation_config)
+        data_validation.validate_all_columns()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<")
+        obj = DataIngestionValidationPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<")
+    except Exception as e:
+        logger.exception(e)
