b875b8f008f4c69a0b3ce9d02b870160a6ec8aa5
Sengarofficial <grv.m02@gmail.com>
2023-10-13 16:28:02 +0530

    Pipeline data cleaning added

---
+++ src/Mlflow_Project/pipeline/stage_02_data_cleaning.py
@@ -0,0 +1,29 @@
+# update the pipeline 
+
+from src.Mlflow_Project.config.configuration import ConfigurationManager
+from src.Mlflow_Project.components.data_cleaning import DataCleaning
+from src.Mlflow_Project import logger 
+
+
+STAGE_NAME = "Data Cleaning Stage"
+
+class DataIngestionCleaningPipeline:
+    def __init__(self):
+        pass 
+
+
+    def main(self):
+        config = ConfigurationManager()
+        data_cleaning_config = config.data_cleaning_config()
+        data_cleaning = DataCleaning(config = data_cleaning_config)  
+        data_cleaning.data_cleaning()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<")
+        obj = DataIngestionCleaningPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<")
+    except Exception as e:
+        logger.exception(e)
