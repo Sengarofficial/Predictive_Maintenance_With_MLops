1937ad69a1400fc4e81d6f058ba5ecbfe6e19fda
Sengarofficial <grv.m02@gmail.com>
2023-10-13 16:26:35 +0530

    Pipeline data ingestion added

---
+++ src/Mlflow_Project/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,29 @@
+# update the pipeline 
+
+from src.Mlflow_Project.config.configuration import ConfigurationManager
+from src.Mlflow_Project.components.data_ingestion import DataIngestion
+from src.Mlflow_Project import logger 
+
+STAGE_NAME = "Data Ingestion Stage"
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass 
+
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<")
+    except Exception as e:
+        logger.exception(e)
